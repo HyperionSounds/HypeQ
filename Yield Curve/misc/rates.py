9f973d63c521280d39f31cca3fbb1e4d0cc7e393
HyperionSounds <drkhllmn@gmail.com>
2022-03-21 21:31:31 -0700

    rates
    rates

--- Yield Curve/misc/rates.py
+++ Yield Curve/misc/rates.py
@@ -8,6 +8,8 @@ import pandas as pd
 import numpy as np
 import datetime
 
+import yfinance as yf
+
 #much of this script is based on code provided here: https://www.viralml.com/static/code/ViralML-Hands-on-Inverted-Yield-Curve.html
 #must download treasury data from the feds website via xml scraper UST, and import CSV to working directory, or from one of the garbage sources commented out below
 
@@ -26,24 +28,24 @@ print(rates_df.head())
 
 # download rates.csv from ust folder package.
 rates_df = pd.read_csv('rates.csv')
-rates_df.columns = ['date', '1M', '3M', '6M', '1Y', '2Y', '3Y', '5Y', '7Y', '10Y', '20Y', '30Y', '30Y Display']
+rates_df.columns = ['Date', '1M', '3M', '6M', '1Y', '2Y', '3Y', '5Y', '7Y', '10Y', '20Y', '30Y', '30Y Display']
 
 recent_rates = rates_df.tail
 
 rates = recent_rates
-print(rates)
+print(rates_df)
 
 fig = plt.figure(figsize=(16, 8))
-plt.plot(rates_df['date'],rates_df['10Y'])
-plt.plot(rates_df['date'],rates_df['5Y'])
-plt.plot(rates_df['date'],rates_df['2Y'])
+plt.plot(rates_df['Date'],rates_df['10Y'])
+plt.plot(rates_df['Date'],rates_df['5Y'])
+plt.plot(rates_df['Date'],rates_df['2Y'])
 plt.suptitle('rates_df')
 
 # Now going to compare long vs short rates and the delta (risk)
 # join both datasets together
 fig, ax = plt.subplots(figsize=(16, 8))
 
-plt.plot(rates_df['date'], rates_df['10Y'] - rates_df['2Y'] , color='r', label='Long minus Short Rates')
+plt.plot(rates_df['Date'], rates_df['10Y'] - rates_df['2Y'] , color='r', label='Long minus Short Rates')
 
 plt.legend()
 plt.grid()
@@ -51,21 +53,46 @@ plt.axhline(0)
 plt.show()
 
 
-#if you have adjusted close data (i.e. zipline)
+#if you have adjusted close data 
+
+# Get the data for the SPY (an ETF on the S&P 500 index) and the stock Apple by specifying the stock ticker, start Date, and end Date
+#mkt_data = yf.download(['SPY', 'AAPL'],'2020-01-01','2022-03-18')
+mkt_data = yf.download('SPY','2020-01-01','2022-03-18')
+
+# Plot the adjusted close prices
+mkt_data["Adj Close"].plot()
+plt.show()
+
+print(mkt_data.tail)
+
+
 """
+# this stuff below is supposed to merge the mkt data and the rates so we can compare. It doesnt work yet
+mkt_data.set_index(pd.to_datetime(mkt_data['Date']), inplace=True)
+rates_df.set_index(pd.to_datetime(rates_df['Date']), inplace=True)
+
+# join both datasets together (if you were to have timeseries of stock / mkt index data)
+together = pd.merge(mkt_data[['Date', 'Adj Close']],
+                    rates_df[['Date', '3Y', '5Y']],
+                    on= ['Date'], how='left')
+
 # Get second axis
 ax2 = ax.twinx()
-plt.plot(together['date'],
+plt.plot(together['Date'],
          together['Adj Close']
          , 'c', label='S&P 500')
 plt.legend()
 plt.title('Rates VS S&P 500')
 ax2.tick_params('vals', colors='b')
 
-# join both datasets together (if you were to have timeseries of stock / mkt index data)
-together = pd.merge(gspc_df[['date', 'Adj Close']],
-                    rates_df[['date', '3Y', '5Y']],
-                    on= ['Date'], how='left')
+
+# last valid observation forward
+together = together.fillna(method='ffill')
+
+# drop NAs
+together = together.dropna(axis=0)
+
+together.tail(20)
 
 
 # get percent change for all interested values
@@ -76,14 +103,14 @@ together.head()
 
 tmp = together.copy()
 cut_off_date = '1990-01-01'
-tmp = tmp[tmp['date'] > cut_off_date]
+tmp = tmp[tmp['Date'] > cut_off_date]
 
 tmp['diff'] = tmp['5 YR'] - tmp['3 YR']
 
 # join both datasets together
 fig, ax = plt.subplots(figsize=(16, 8))
 
-plt.plot(tmp['date'], tmp['diff'].rolling(window=5).mean().values,
+plt.plot(tmp['Date'], tmp['diff'].rolling(window=5).mean().values,
          color='r',
          linewidth=1, label='       Rates PCT')
 plt.legend()
@@ -100,14 +127,14 @@ ax.pcolorfast(ax.get_xlim(), ax.get_ylim(),
 
 # Get second axis
 ax2 = ax.twinx()
-plt.plot(tmp['date'],
+plt.plot(tmp['Date'],
          tmp['Adj Close'].rolling(window=5).mean().values
          , 'c--', label='S&P 500 PCT')
 plt.legend()
 plt.title('Rates Diff VS S&P 500')
 ax2.tick_params('vals', colors='b')
 """
-
+"""
 
 #formatter 
 """
